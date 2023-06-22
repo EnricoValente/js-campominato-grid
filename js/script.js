e6b082464a3650d881483c49368bd0763b06bc11
EnricoValente <enricovalente42@gmail.com>
2023-06-22 15:18:58 +0200

    definita funzione per la tabella

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
 
 
-const container = document.querySelector('.container');
 
 const playButton = document.querySelector('button');
 
 playButton.addEventListener('click', function() {
+   
+    myBoard();
+})
+   
+        
+        
+   
     
+    
+
+
+
+function myBoard() {
+
     for(let i = 1; i <= 100; i++) {
+        const container = document.querySelector('.container');
         const square = document.createElement('div');
         square.classList.add('square');
         square.append(i);
         container.append(square);
         console.log('square', square, typeof square);
-        
-        
+
         square.addEventListener('click', function() {
             square.classList.toggle('clicked');
             console.log(this.innerHTML);
         })
     
     }
-})
+}
